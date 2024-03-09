4bd9d133f7035d0fa102b1b384158e3053d9892f
Paulina Vazquez Encina <vazquez.encina.paulina@gmail.com>
2024-03-09 15:24:06 -0600

    Creacion y primer avance de las clases empleados y categorias.

---
+++ src/edu/paulina_vazquez/reto6/data/Categoria.java
@@ -0,0 +1,47 @@
+package edu.paulina_vazquez.reto6.data;
+
+public class Categoria {
+    private String nombreDeCategoria;
+    private String claveDeCategoria;
+    private double sueldoBase;
+    private double pagoHoraExtra;
+
+    public Categoria(String nombreDeCategoria, String claveDeCategoria, double sueldoBase, double pagoHoraExtra) {
+        this.nombreDeCategoria = nombreDeCategoria;
+        this.claveDeCategoria = claveDeCategoria;
+        this.sueldoBase = sueldoBase;
+        this.pagoHoraExtra = pagoHoraExtra;
+    }
+
+    public String getnombreDeCategoria() {
+        return nombreDeCategoria;
+    }
+
+    public void setnombreDeCategoria(String nombreDeCategoria) {
+        this.nombreDeCategoria = nombreDeCategoria;
+    }
+
+    public String getClave() {
+        return claveDeCategoria;
+    }
+
+    public void setClave(String clave) {
+        this.claveDeCategoria = claveDeCategoria;
+    }
+
+    public double getSueldoBase() {
+        return sueldoBase;
+    }
+
+    public void setSueldoBase(double sueldoBase) {
+        this.sueldoBase = sueldoBase;
+    }
+
+    public double getPagoHoraExtra() {
+        return pagoHoraExtra;
+    }
+
+    public void setPagoHoraExtra(double pagoHoraExtra) {
+        this.pagoHoraExtra = pagoHoraExtra;
+    }
+}
