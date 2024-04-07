c4d88a0f8718b091ca0a824f13fce0c0e03f8b51
Paulina Vazquez Encina <vazquez.encina.paulina@gmail.com>
2024-04-06 22:03:22 -0600

    Documentacion de las clases de process.

--- src/edu/paulina_vazquez/reto9/process/LineCleaner.java
+++ src/edu/paulina_vazquez/reto9/process/LineCleaner.java
@@ -5,13 +5,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LineCleaner {
+    /**
+     * Esta clase se encarga de limpar las cadenas mediante
+     * la eliminación de los caracteres especificados en las
+     * indicaciones.
+     */
     public static String limpiarLinea(String linea) {
+        /**
+         * Convierte la línea a minúsculas y elimina caracteres no alfabéticos y acentos,
+         * y elimina los caracteres especiales y acentos que no son ASCII.
+         */
         linea = linea.toLowerCase().replaceAll("[^\\p{IsAlphabetic}\\s]", "");
         linea = Normalizer.normalize(linea, Normalizer.Form.NFD);
         return linea.replaceAll("[^\\p{ASCII}]", "");
     }
 
     public static List<String> limpiarLineas(List<String> lineas) {
+        /**
+         * Limpia una lista de líneas de texto eliminando caracteres
+         * no alfabéticos y convirtiendo el texto a minúsculas.
+         */
         List<String> lineasLimpias = new ArrayList<>();
         for (String linea : lineas) {
             lineasLimpias.add(limpiarLinea(linea));
--- src/edu/paulina_vazquez/reto9/process/WordManager.java
+++ src/edu/paulina_vazquez/reto9/process/WordManager.java
@@ -2,35 +2,54 @@ package edu.paulina_vazquez.reto9.process;
 
 import edu.paulina_vazquez.reto9.ui.Textos;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 
 public class WordManager {
-
+    /**
+     * Esta clase se encarga de separar, contar y obtener las palabras
+     * mas repetidos del archivos seleccionado por el ususario.
+     */
     private Map<String, Integer> mapaPalabras;
 
     public WordManager() {
+        /**
+         * Constructor de la clase WordManager. Inicializa un nuevo mapa de palabras vacío.
+         */
         mapaPalabras = new HashMap<>();
     }
 
     public void contarPalabras(String archivo) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(archivo));
-        String linea;
-        while ((linea = reader.readLine()) != null) {
-            linea = LineCleaner.limpiarLinea(linea);
-            String[] palabras = linea.split("\\s+");
-            for (String palabra : palabras) {
-                mapaPalabras.put(palabra, mapaPalabras.getOrDefault(palabra, 0) + 1);
+        /**
+         * Lee el contenido de un archivo de texto y cuenta la frecuencia de cada palabra.
+         * @param archivo Ruta del archivo de texto a analizar.
+         * @throws IOException Si ocurre un error al leer el archivo.
+         */
+        try (InputStream inputStream = getClass().getResourceAsStream("/resources/" + archivo)) {
+            if (inputStream != null) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                String linea;
+                while ((linea = reader.readLine()) != null) {
+                    linea = LineCleaner.limpiarLinea(linea);
+                    String[] palabras = linea.split("\\s+");
+                    for (String palabra : palabras) {
+                        mapaPalabras.put(palabra, mapaPalabras.getOrDefault(palabra, 0) + 1);
+                    }
+                }
+            } else {
+                throw new FileNotFoundException("Archivo no encontrado: " + archivo);
             }
         }
-        reader.close();
     }
 
     public void imprimirPalabrasMasRepetidas(Textos textos) {
-        PriorityQueue<Map.Entry<String, Integer>> heap = new PriorityQueue<>(Comparator.comparingInt(Map.Entry::
-                getValue));
+
+        /**
+         * Imprime en pantalla las 10 palabras más repetidas junto con su frecuencia.
+         * @param textos Objeto que contiene los textos para la impresión en el idioma seleccionado.
+         */
+
+        PriorityQueue<Map.Entry<String, Integer>> heap = new PriorityQueue<>(Comparator.comparingInt(Map.Entry::getValue));
         for (Map.Entry<String, Integer> entry : mapaPalabras.entrySet()) {
             heap.offer(entry);
             if (heap.size() > 10) {
